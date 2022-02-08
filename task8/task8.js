5987ff04c355789fd945eb256d711e228fe770bf
Cazacu-Condart Dumitru <71759697+Cazacu-Condrat-Dumitru@users.noreply.github.com>
2022-02-08 19:54:59 +0200

    Add files via upload

---
+++ task8/task8.js
@@ -0,0 +1,15 @@
+let userName = prompt('Write your username');
+let password = prompt('Write Your password');
+
+if (userName === 'dima') {
+  alert("Welcome Dima");
+}
+else {
+  alert("Wrong username");
+}
+
+if (password === "dima123") {
+  alert("Welcome back Dima")
+} else {
+  alert("Wrong password")
+}
