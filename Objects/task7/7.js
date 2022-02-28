38036bf22efafb4f150ed65cf990ace2ee2d549a
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2022-03-01 01:18:29 +0200

    task fix

--- Objects/task7/7.js
+++ Objects/task7/7.js
@@ -14,37 +14,36 @@
 // Создайте по одной функции get для каждого поля объекта (функции/методы должныдолны принадлежать объекту)
 
 let person = {
-  name: 'Person',
-  age: 123,
-  salary: 1542.33,
-  contacts: {
-    phone: '112',
-    email: 'email@domain.com'
-  },
-  address: 'Moldova',
+    name: 'Person',
+    age: 123,
+    salary: 1542.33,
+    contacts: {
+        phone: '112',
+        email: 'email@domain.com'
+    },
+    address: 'Moldova',
 
 
-  get nameUser() {
-    this.name = 'Dima';
-    return this.name;
-  },
-  get ageUser() {
-    age = 20;
-    return this.age;
-  },
-  get salaryUser() {
-    salary = 0;
-    return this.salary;
-  },
-  get contactsUser() {
-    phoneconNew = '+37369336576';
-    emailNew = 'email@gmail.com';
-    this.contacts.phone = phoneconNew;
-		this.contacts.email = emailNew;
-  },
-  get addressUser() {
-    address = 'Alexandru cel Bun';
-    return this.address;
-  }
+    get nameUser() {
+        console.log(this.name);
+    },
+    get ageUser() {
+        console.log(this.age);
+    },
+    get salaryUser() {
+        console.log(this.salary);
+    },
+    get contactsUser() {
+        console.log(this.contacts);
+    },
+    get phoneUser() {
+        console.log(this.phone);
+    },
+    get emailUser() {
+        console.log(this.email);
+    },
+    get addressUser() {
+        console.log(this.address);
+    }
 };
 console.log(person);
