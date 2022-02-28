15cb504b223e9150661fbde531778b64eed50237
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2022-03-01 00:08:11 +0200

    deleted all files and moved all to folder

--- task 4/task4.js
+++
@@ -1,5 +0,0 @@
-const ucFirst = function (str){
-	alert(`Initial Word: ${str}
-Changed word: ${str[0].toUpperCase() + str.slice(1)}`);
-}
-ucFirst(prompt('Write your world without capitalize'));
--- task 5/task5.js
+++
@@ -1,3 +0,0 @@
-let extractCurrencyValue = (str) => +str.slice(1);
-
-alert(extractCurrencyValue('$120') === 120);
