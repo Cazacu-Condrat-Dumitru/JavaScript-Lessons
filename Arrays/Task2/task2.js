6ff24c9e33dd9463e6e71113243fb118ebf59964
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2022-03-01 02:53:39 +0200

    fixed task2

--- Arrays/Task2/task2.js
+++ Arrays/Task2/task2.js
@@ -1,28 +1,27 @@
-const list = [
-    {
-        name: "Dima",
-        marks: [1, 2, 3, 4, 5, 2, 3]
-    },
-    {
-        name: "Ion",
-        marks: [4, 2, 2, 7, 1, 10]
-    },
-    {
-        name: "Cristina",
-        marks: [6, 7, 8, 9]
-    },
-    {
-        name: "Alex",
-        marks: [9, 9, 9, 9, 9]
-    },
-    {
-        name: "Max",
-        marks: [4, 8, 4, 2, 10, 9, 9, 9]
-    }
+const list = [{
+            name: "Dima",
+            marks: [1, 2, 3, 4, 5, 2, 3]
+        },
+        {
+            name: "Ion",
+            marks: [4, 2, 2, 7, 1, 10]
+        },
+        {
+            name: "Cristina",
+            marks: [6, 7, 8, 9]
+        },
+        {
+            name: "Alex",
+            marks: [9, 9, 9, 9, 9]
+        },
+        {
+            name: "Max",
+            marks: [4, 8, 4, 2, 10, 9, 9, 9]
+        }
 
-]
-//The reduce() method executes a reducer function on each element of the array
-//resulting in a single output value.
+    ]
+    //The reduce() method executes a reducer function on each element of the array
+    //resulting in a single output value.
 
 const average = list.map(result => ({
     name: result.name,
@@ -34,13 +33,30 @@ console.log(average);
 const resultLowerAverage = average.filter(item => item.avg < 5);
 console.log(`Students with average mark lower than 5:`, resultLowerAverage);
 
-//Finding the student with the highest and lower grade
-const [max] = list;
-Math.max(...list.map(item => item.marks));
-console.log(`The student with the highest grade is:`, max);
+const max = (average) => {
+    let max = 0;
+    let a = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (max < average[i].avg) {
+            a = i;
+            max = average[i].avg;
+        }
+    }
+    console.log(`The student with the highest grade is:`, average[a]);
+}
 
-const [min] = list;
-Math.min(...list.map(item => item.marks));
-console.log(`The student with thw lowest grade is:`, min);
-//Sorting the students(the highest grade at the beginning lowest at the end)
-console.log(average.sort((item, index) => index.avg - item.avg));
+max(average);
+
+const min = (average) => {
+    let min = 10;
+    let a = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (min > average[i].avg) {
+            a = i;
+            min = average[i].avg;
+        }
+    }
+    console.log(`The student with the  minimal is:`, average[a]);
+}
+
+min(average);
