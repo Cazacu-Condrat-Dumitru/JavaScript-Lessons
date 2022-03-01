5c49e04301382b62f45d4b8d822b267a37e254a6
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2022-03-01 23:19:30 +0200

    task10

---
+++ Objects/task10/10.js
@@ -0,0 +1,38 @@
+// Задача №10 - работа с ключами объекта
+
+// Создайте 2 объект person со следующей структурой:
+// {
+// 	name: `Person`,
+// 	age: 123,
+// 	salary: 1542.33,
+// 	contacts: {
+// 		phone: `112`,
+// 		email: ‘email@domain.com’
+// },
+// address: `Moldova`
+// }
+
+// Напишите метод Intersection который получит на входе эти 2 объекта и вернет новый объект только из тех полей
+//  значение которых равна о обоих объектов.
+
+let person1 = {
+    name: 'Person',
+    age: 123,
+    salary: 1542.33,
+    contacts: {
+        phone: '112',
+        email: 'email@domain.com'
+    },
+    address: 'Moldova'
+};
+
+let person2 = {
+    name: 'Person',
+    age: 123,
+    salary: 1542.33,
+    contacts: {
+        phone: '112',
+        email: 'email@domain.com'
+    },
+    address: 'Moldova'
+};
